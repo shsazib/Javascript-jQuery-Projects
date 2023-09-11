5e7b057d9de450f6b32bea640d403951da8b1e93
shsazib <shsazibbhuiyan892939@gmail.com>
2023-09-11 19:11:23 +0600

    complate live weather app

--- Javascript/live-weather/script.js
+++ Javascript/live-weather/script.js
@@ -1,25 +1,48 @@
 const apiKey = "43c7fc90ee462969ea43b1fe1167b607";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchInput = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather_icon");
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
-    console.log(data);
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+  console.log(data);
 
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = data.main.temp + "°c";
-    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
-    document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
-    searchInput.value='';
+  searchInput.value = "";
 
-    document.querySelector('.weather').style.display = "block";
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
+
+  document.querySelector(".weather").style.display = "block";
 }
 
+searchInput.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    if (searchInput.value != "") {
+      checkWeather(searchInput.value);
+    }
+  }
+});
 
-searchBtn.addEventListener('click', () =>{
+searchBtn.addEventListener("click", () => {
+  if (searchInput.value != "") {
     checkWeather(searchInput.value);
-})
+  }
+});
