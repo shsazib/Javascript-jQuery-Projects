2ed5665acccb5fab903ec2b1e3b42d220175f404
shsazib <shsazibbhuiyan892939@gmail.com>
2023-09-14 16:19:05 +0600

    qorking with quiz app

--- Javascript/quiz-app/script.js
+++ Javascript/quiz-app/script.js
@@ -1,5 +1,31 @@
+const questions = [
+    {
+        question : "Whish is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question : "Whish is the Smallest Continent in the world?",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    },
+]
 
 
+const questionElement = document.querySelector('#question');
+const answerButton = document.querySelector('#answer_btn');
+const nextButton = document.querySelector('#next_btn');
+
+let currentQuestionIndex = 0;
+let score = 0;
 
 
-// localStorage.clear();
+questionElement.innerHTML = questions[0].question;
